19820edbd0b26c55844b5f464e6fec8e68a0b493
prakhar <prakhar.mpcz@gmail.com>
2026-03-18 16:50:08 +0530

    login form removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './Dashboard';
-import LoginForm from './LoginForm';
+// import LoginForm from './LoginForm';
 // import LoginForm from './LoginForm';
 
 function App() {
-  const isLoggedIn = localStorage.getItem("is_superuser");
-
+  // const isLoggedIn = localStorage.getItem("is_superuser");
+  // {isLoggedIn ?  : <LoginForm />}
   return (
     <div className="App">
-      {isLoggedIn ? <Dashboard /> : <LoginForm />}
+      <Dashboard />
     </div>
   );
 }
